b0bbac8b84cfae61c3ef447696250f3697425c71
Cheng JIANG <jiang.cheng@vip.163.com>
2020-06-14 16:50:28 +0200

    remove timeout which causes problems

--- vim/install.sh
+++ vim/install.sh
@@ -6,14 +6,14 @@ DISTRO="$(lsb_release -is)"
 NODE_VERSION="v10.15.3"
 
 if [ $DISTRO != "Deepin" ] && [ $DISTRO != "Ubuntu" ] && [ $DISTRO != "Linuxmint" ]; then
-    echo "Error: distribution is not one of (deepin, ubuntu, LinuxMint)" && exit 1
+    echo "Error: distribution is not one of (deepin, ubuntu, linuxmint)" && exit 1
 fi
 
 function echoc() {
     echo -e "$(tput setaf 2; tput bold)$1$(tput sgr0)"
 }
 
-deps=("curl" "git" "neovim" "vim" "build-essential" "cmake" "python3-dev" "python3-pip" "tmux" "clang-format" "autoconf" "automake" "cppcheck" "flake8" "pylint" "ruby" "ruby-dev" "rust-lldb" "lldb" "apt-file" "openssh-server" "jq" "zsh" "yapf3")
+deps=("curl" "git" "neovim" "vim" "build-essential" "cmake" "python3-dev" "python3-pip" "tmux" "clang-format" "autoconf" "automake" "cppcheck" "flake8" "pylint" "ruby" "ruby-dev" "rust-lldb" "lldb" "apt-file" "openssh-server" "jq" "zsh" "yapf3" "libssl-dev")
 echoc "=> Installing dependencies..."
 for dep in "${deps[@]}"
 do
@@ -22,66 +22,66 @@ done
 
 echoc "=> Installing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --retry-delay 2 --retry 3
 
 echoc "=> Installing onedark.vim..."
 curl -fLo ~/.vim/autoload/onedark.vim --create-dirs \
-    https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim --retry-delay 2 --retry 3
 curl -fLo ~/.vim/colors/onedark.vim --create-dirs \
-    https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim --retry-delay 2 --retry 3
 
 echoc "=> Installing monokai.vim..."
 curl -fLo ~/.vim/colors/monokai.vim --create-dirs \
-    https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim --retry-delay 2 --retry 3
 
 echoc "=> Installing one.vim..."
 curl -fLo ~/.vim/colors/one.vim --create-dirs \
-    https://raw.githubusercontent.com/rakr/vim-one/master/colors/one.vim -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/rakr/vim-one/master/colors/one.vim --retry-delay 2 --retry 3
 curl -fLo ~/.vim/autoload/airline/themes/one.vim --create-dirs \
-    https://raw.githubusercontent.com/rakr/vim-one/master/autoload/airline/themes/one.vim -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/rakr/vim-one/master/autoload/airline/themes/one.vim --retry-delay 2 --retry 3
 
 echoc "=> Installing sass-convert (scss formatter)..."
 sudo gem install sass
 
 echoc "=> Installing shfmt..."
 if [ ! -f ~/Downloads/shfmt_v3.0.0_linux_amd64 ]; then
-    curl -fLo ~/Downloads/shfmt_v3.0.0_linux_amd64 https://github.com/mvdan/sh/releases/download/v3.0.0/shfmt_v3.0.0_linux_amd64 -m 15 --retry-delay 2 --retry 3 \
+    curl -fLo ~/Downloads/shfmt_v3.0.0_linux_amd64 https://github.com/mvdan/sh/releases/download/v3.0.0/shfmt_v3.0.0_linux_amd64 --retry-delay 2 --retry 3 \
         && sudo cp ~/Downloads/shfmt_v3.0.0_linux_amd64 /usr/local/bin/shfmt \
         && sudo chmod u+x /usr/local/bin/shfmt
 fi
 
 echoc "=> Configuring tern..."
 curl -fLo ~/.tern-config --create-dirs \
-    https://raw.githubusercontent.com/GopherJ/cfg/master/tern/.tern-config -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/GopherJ/cfg/master/tern/.tern-config --retry-delay 2 --retry 3
 
 echoc "=> Configuring tmux..."
 curl -fLo ~/.tmux.conf --create-dirs \
-    https://raw.githubusercontent.com/GopherJ/cfg/master/tmux/.tmux.conf -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/GopherJ/cfg/master/tmux/.tmux.conf --retry-delay 2 --retry 3
 curl -fLo ~/.tmuxline_snapshot --create-dirs \
-    https://raw.githubusercontent.com/GopherJ/cfg/master/tmux/.tmuxline_snapshot -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/GopherJ/cfg/master/tmux/.tmuxline_snapshot --retry-delay 2 --retry 3
 
 echoc "=> Configuring eslint..."
 curl -fLo ~/.eslintrc.js --create-dirs \
-    https://raw.githubusercontent.com/GopherJ/cfg/master/eslint/.eslintrc.js -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/GopherJ/cfg/master/eslint/.eslintrc.js --retry-delay 2 --retry 3
 
 echoc "=> Configuring stylint..."
 curl -fLo ~/.stylelintrc --create-dirs \
-    https://raw.githubusercontent.com/GopherJ/cfg/master/stylelint/.stylelintrc -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/GopherJ/cfg/master/stylelint/.stylelintrc --retry-delay 2 --retry 3
 
 echoc "=> Configuring ctags..."
 curl -fLo ~/.ctags.d/makefile.ctags --create-dirs \
-    https://raw.githubusercontent.com/GopherJ/cfg/master/ctags/.ctags.d/makefile.ctags -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/GopherJ/cfg/master/ctags/.ctags.d/makefile.ctags --retry-delay 2 --retry 3
 curl -fLo ~/.ctags.d/ansible.ctags --create-dirs \
-    https://raw.githubusercontent.com/GopherJ/cfg/master/ctags/.ctags.d/ansible.ctags -m 15 --retry-delay 2 --retry 3
+    https://raw.githubusercontent.com/GopherJ/cfg/master/ctags/.ctags.d/ansible.ctags --retry-delay 2 --retry 3
 
 echoc "=> Configuring oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
-    && curl https://raw.githubusercontent.com/GopherJ/cfg/master/zshrc/.zshrc -m 15 --retry-delay 2 --retry 3 >> ~/.zshrc \
+    && curl https://raw.githubusercontent.com/GopherJ/cfg/master/zshrc/.zshrc --retry-delay 2 --retry 3 >> ~/.zshrc \
     && source ~/.zshrc
 
 echoc "=> Installing golang1.14.4..."
 if [ ! -f ~/Downloads/go1.14.4.linux-amd64.tar.gz ]; then
-    curl -fLo ~/Downloads/go1.14.4.linux-amd64.tar.gz https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz -m 15 --retry-delay 2 --retry 3 \
+    curl -fLo ~/Downloads/go1.14.4.linux-amd64.tar.gz https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz --retry-delay 2 --retry 3 \
         && sudo tar -C /usr/local -xzf ~/Downloads/go1.14.4.linux-amd64.tar.gz
 fi
 
@@ -102,7 +102,7 @@ fi
 
 command -v nvm > /dev/null || {
 echoc "=> Installing nvm..." \
-    && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh -m 15 --retry-delay 2 --retry 3 | bash \
+    && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh --retry-delay 2 --retry 3 | bash \
     && export NVM_DIR="$HOME/.nvm" \
     && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" \
     && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
@@ -128,7 +128,7 @@ echoc "=> Installing yarn, vue, js language server, eslint, markdown render, bea
     && npm i -g stylelint stylelint-processor-styled-components stylelint-config-styled-components stylelint-config-recommended
 
 echoc "=> Configuring vim and building YCM..."
-curl -fo ~/.vimrc https://raw.githubusercontent.com/GopherJ/cfg/master/vim/.vimrc -m 15 --retry-delay 2 --retry 3 \
+curl -fo ~/.vimrc https://raw.githubusercontent.com/GopherJ/cfg/master/vim/.vimrc --retry-delay 2 --retry 3 \
     && sed -i "s/v10.15.3/$NODE_VERSION/g" ~/.vimrc \
     && vim -c "PlugInstall" ~/.vimrc \
     && cd ~/.vim/plugged/YouCompleteMe \
@@ -145,7 +145,7 @@ fi
 
 command -v rustup > /dev/null || {
 echoc "=> Installing rustup toolchain..." \
-    && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -m 15 --retry-delay 2 --retry 3 | sh
+    && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --retry-delay 2 --retry 3 | sh
 }
 
 echoc "=> Configuring rust..." \
@@ -190,7 +190,7 @@ fi
 echoc "=> Installing ripgrep..."
 command -v rg > /dev/null || {
     if [ ! -f  ~/Downloads/ripgrep_11.0.2_amd64.deb ]; then
-        curl -fLo ~/Downloads/ripgrep_11.0.2_amd64.deb https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb -m 15 --retry-delay 2 --retry 3 \
+        curl -fLo ~/Downloads/ripgrep_11.0.2_amd64.deb https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb --retry-delay 2 --retry 3 \
             && sudo dpkg -i ~/Downloads/ripgrep_11.0.2_amd64.deb
     fi
 }
