f86f0e30ff95a1b0e6809cb2e1892fb4ce305226
Cheng JIANG <jiang.cheng@vip.163.com>
2020-06-20 17:06:09 +0200

    add fcitx

---
+++ fcitx/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt remove --purge ibus
+sudo apt remove --purge indicator-keyboard
+sudo apt install -y fcitx-table-wbpy fcitx-config-gtk
+im-config -n fcitx
+
+if [ ! -f ~/Downloads/sogoupinyin_2.2.0.0108_amd64.deb ]; then
+    curl -O http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_2.2.0.0108_amd64.deb?st=ryCwKkvb-0zXvtBlhw5q4Q&e=1529739124&fn=sogoupinyin_2.2.0.0108_amd64.deb
+fi
+
+sudo dpkg -i ~/Download/sogoupinyin_2.2.0.0108_amd64.deb
+sudo apt -f install
+
+fcitx-config-gtk3
+sudo shutdown -r now
