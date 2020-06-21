76935bc1f9e706504251ed7ca7ebe89ac29cd693
Cheng JIANG <jiang.cheng@vip.163.com>
2020-06-21 18:48:23 +0200

    fix deb download location

--- fcitx/install.sh
+++ fcitx/install.sh
@@ -6,7 +6,7 @@ sudo apt install -y fcitx-table-wbpy fcitx-config-gtk
 im-config -n fcitx
 
 if [ ! -f ~/Downloads/sogoupinyin_2.2.0.0108_amd64.deb ]; then
-    curl -O http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_2.2.0.0108_amd64.deb?st=ryCwKkvb-0zXvtBlhw5q4Q&e=1529739124&fn=sogoupinyin_2.2.0.0108_amd64.deb
+    curl -fLo ~/Downloads/sogoupinyin_2.2.0.0108_amd64.deb http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_2.2.0.0108_amd64.deb?st=ryCwKkvb-0zXvtBlhw5q4Q&e=1529739124&fn=sogoupinyin_2.2.0.0108_amd64.deb
 fi
 
 sudo dpkg -i ~/Download/sogoupinyin_2.2.0.0108_amd64.deb
