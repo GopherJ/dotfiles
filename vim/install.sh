19265d6172a9f5d665d87ebe4a2ff35832b12ab7
Cheng JIANG <jiang.cheng@vip.163.com>
2020-09-19 14:25:38 +0200

    add bottom

--- vim/install.sh
+++ vim/install.sh
@@ -388,6 +388,7 @@ echoc "=> Configuring rust..." \
     && cargo install sccache \
     && cargo install mdcat \
     && cargo install clog-ci \
+    && cargo install --git https://github.com/ClementTsang/bottom \
     && cargo install --git https://github.com/brson/basic-http-server \
     && cargo install --git  https://github.com/zesterer/tao \
     && cargo install cargo-outdated \
