de3351847436be6fa554069c7205459e079ac545
Cheng JIANG <jiang.cheng@vip.163.com>
2020-11-03 19:26:31 +0100

    add trust-CA.sh

---
+++ scripts/trust-CA.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -z "$1" ]; then
+    echo "Usage: ./trust-CA.sh cacert.pem" && exit 1
+fi
+
+CA_CERT_PATH="$1"
+CA_CERT="$(basename "$CA_CERT_PATH")"
+
+sudo apt install ca-certificates libnss3-tools
+
+if [ ! -d /usr/share/ca-certificates/selfsigned ]; then
+    sudo mkdir /usr/share/ca-certificates/selfsigned
+fi
+
+
+sudo cp $CA_CERT_PATH /usr/share/ca-certificates/selfsigned
+
+sudo dpkg-reconfigure ca-certificates
+
+certutil -d sql:$HOME/.pki/nssdb \
+    -A \
+    -t "C,," \
+    -n "My Homemade CA" \
+    -i /usr/share/ca-certificates/selfsigned/$CA_CERT
+
+sudo update-ca-certificates
