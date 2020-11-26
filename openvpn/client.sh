7926d5ef31309447425756210b1399d8592ec884
Cheng JIANG <jiang.cheng@vip.163.com>
2020-11-26 09:48:07 +0100

    add openvpn example scripts

---
+++ openvpn/client.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+command -v openvpn > /dev/null || {
+    sudo apt install -y openvpn
+}
+
+if [ $# -lt 2 ]; then
+    echo "No identity OR openvpn server provided but required for generating client certificates" && exit 1
+fi
+
+echo "Have you already initiated pki and built ca certificates? (y/n)"
+read -p ">" answer
+
+if [ "$answer" != "y" ]; then
+    echo "Please run ./server.sh first" && exit 1
+fi
+
+cd /etc/openvpn
+
+if [ ! -f ./easyesa ]; then
+    echo "No easyesa found" && exit 1
+fi
+
+sudo ./easyesa build-client-full $1 nopass
+
+
+if [ ! -f "$HOME/openvpn-client-$1" ]; then
+    mkdir "$HOME/openvpn-client-$1"
+fi
+
+sudo cp ./pki/ca.crt $HOME/openvpn-client-$1
+sudo cp ./pki/issued/$1.crt $HOME/openvpn-client-$1
+sudo cp ./pki/private/$1.crt $HOME/openvpn-client-$1
+
+sudo chown -R $USER:$USER $HOME/openvpn-client-$1
+
+cat > $HOME/openvpn-client-$1/client.conf <<-EOF
+dev tap
+proto udp
+remote $2 1194
+remote-cert-tls server
+client
+nobind
+ca ./ca.crt
+cert ./$1.crt
+key ./$1.key
+persist-key
+persist-tun
+comp-lzo yes
+daemon
+EOF
+
+echo "Profiles generated in $HOME/openvpn-client-$1"
+echo "You can connect your server by running:"
+echo "\t cd $HOME/openvpn-client-$1 && openvpn --config client.conf"
---
+++ openvpn/server.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+command -v openvpn > /dev/null || {
+    sudo apt install -y openvpn
+}
+
+if [ ! -f ~/Downloads/v3.0.6.tar.gz ]; then
+    curl -fLo ~/Downloads/v3.0.6.tar.gz https://github.com/OpenVPN/easy-rsa/archive/v3.0.6.tar.gz
+fi
+
+cd ~/Downloads
+
+tar -zxvf v3.0.6.tar.gz
+
+sudo cp -r easy-rsa-3.0.6/easyrsa3/* /etc/openvpn
+
+cd /etc/openvpn
+
+sudo ./easyrsa init-pki
+sudo ./easyrsa build-ca nopass
+sudo ./easyrsa gen-dh
+sudo ./easyrsa build-server-full server nopass
+
+if [ ! -f server ]; then
+    sudo mkdir /etc/openvpn/server
+fi
+
+if [ ! -f client ]; then
+    sudo mkdir /etc/openvpn/client
+fi
+
+cat > ./server/server.conf  <<-EOF
+daemon
+port 1194
+proto udp
+dev tap
+
+ca /etc/openvpn/pki/ca.crt
+cert /etc/openvpn/pki/issued/server.crt
+key /etc/openvpn/pki/private/server.key
+dh /etc/openvpn/pki/dh.pem
+
+ifconfig-pool-persist /var/log/openvpn/ipp.txt
+server-bridge 10.8.0.4 255.255.255.0 10.8.0.50 10.8.0.250
+client-to-client
+keepalive 10 120
+
+comp-lzo
+
+persist-key
+persist-tun
+
+up /etc/openvpn/server/up.sh
+
+status /var/log/openvpn/openvpn-server-status.log
+
+log /var/log/openvpn/openvpn-server.log
+
+script-security 2
+verb 3
+EOF
+
+cat > ./server/up.sh <<-EOF
+#!/bin/sh
+/sbin/ifconfig $1 10.8.0.4 netmask 255.255.255.0 broadcast 10.8.0.25
+EOF
+
+sudo chmod u+x ./server/up.sh
+
+sudo openvpn --config /etc/openvpn/server/server.conf
